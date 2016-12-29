212f7d8bcd9f023bb20420ce08c23d65935206ce
cresta white <crestawhite@gmail.com>
2016-12-28 22:35:39 -0500

    video1 w4-d5

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+/*eslint-disable*/
+
+import React from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+
+render(<StorePicker/>, document.querySelector('#main'));
