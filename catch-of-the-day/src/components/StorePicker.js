684cb5f2e70670c45bb5a1a61f9b5c671353a08e
cresta white <crestawhite@gmail.com>
2016-12-28 23:26:47 -0500

    video6 w4d5

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -6,7 +6,7 @@ class StorePicker extends React.Component {
       <form className="store-selector">
       <h2>Please Enter A Sotre</h2>
       <input type="text" required placeholder="Store Name" />
-      <button> type="submit">Visit Store </button>
+      <button type="submit">Visit Store </button>
 
       </form>
        )
