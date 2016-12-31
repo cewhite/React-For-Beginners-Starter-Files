a00e4773210a0c6a30316451e02eee893c83b9fe
cresta white <crestawhite@gmail.com>
2016-12-31 15:39:30 -0500

    w4d5 video9

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+
+
 class App extends React.Component {
   render ( ) {
     return (
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render( ) {
-    return (
+    return (//do not place jsx comments here//
       <form className="store-selector">
-      <h2>Please Enter A Sotre</h2>
+    {/*comment in jsx */}
+      <h2>Please Enter A Store</h2>
       <input type="text" required placeholder="Store Name" />
       <button type="submit">Visit Store </button>
-
       </form>
        )
   }
